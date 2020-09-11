920d884a2a3b349ff3692430a532bc7db2f33d4e
TheNameOfDreams <Blackwindow2109>
2020-09-11 14:59:32 -0300

    first commit

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,12 +5,39 @@ import Header from "../../components/header/index";
 import Banner from "../../components/banner";
 import Footer from "../../components/footer";
 
-import './style.css'
+import "./style.css";
+import api from "../../components/admin/api";
 
 export default class MainPage extends Component {
-  componentDidMount() {
-    document.title = 'Painel Administrativo - SaintMC';
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topBan: {
+        name: "Ninguém",
+        count: "-/-",
+      },
+
+      banCount: 0,
+    };
   }
+
+  componentDidMount() {
+    this.loadInfo();
+    document.title = "Painel Administrativo - SaintMC";
+  }
+
+  loadInfo = async (props) => {
+    let response = await api.get("ban/top");
+
+    this.setState({
+      topBan: { name: response.data.name, count: response.data.count },
+    });
+
+    response = await api.get("ban/count");
+
+    this.setState({ banCount: response.data.count });
+  };
 
   render() {
     return (
@@ -22,36 +49,67 @@ export default class MainPage extends Component {
             <div className="col-md-4">
               <div className="main-cards first-main-card">
                 <div className="main-cards-title">
-                  <h3 className="text-center" style={{marginBottom: "0", fontSize: "1.42rem"}}>Total de jogadores <strong>banidos</strong></h3>
+                  <h3
+                    className="text-center"
+                    style={{ marginBottom: "0", fontSize: "1.42rem" }}
+                  >
+                    Total de jogadores <strong>banidos</strong>
+                  </h3>
                 </div>
                 <div className="main-cards-body">
-                  <p className="text-center"><strong style={{fontSize: "40px", padding: "0 10px"}}>500</strong> banimentos.</p>
+                  <p className="text-center">
+                    <strong style={{ fontSize: "40px", padding: "0 10px" }}>
+                      {this.state.banCount}
+                    </strong>{" "}
+                    banimentos.
+                  </p>
                 </div>
               </div>
             </div>
             <div className="col-md-4">
               <div className="main-cards">
                 <div className="main-cards-title">
-                  <h3 className="text-center" style={{marginBottom: "0", fontSize: "1.42rem"}}>Membro da equipe com mais <strong>banimentos</strong></h3>
+                  <h3
+                    className="text-center"
+                    style={{ marginBottom: "0", fontSize: "1.42rem" }}
+                  >
+                    Membro da equipe com mais <strong>banimentos</strong>
+                  </h3>
                 </div>
                 <div className="main-cards-body">
-                  <p className="text-center">Avontz com <strong style={{fontSize: "40px", padding: "0 10px"}}>500</strong> banimentos.</p>
+                  <p className="text-center">
+                    {this.state.topBan.name} com{" "}
+                    <strong style={{ fontSize: "40px", padding: "0 10px" }}>
+                      {this.state.topBan.count}
+                    </strong>{" "}
+                    banimentos.
+                  </p>
                 </div>
               </div>
             </div>
             <div className="col-md-4">
               <div className="main-cards">
                 <div className="main-cards-title">
-                  <h3 className="text-center" style={{marginBottom: "0", fontSize: "1.42rem"}}>Total de jogadores <strong>silenciados</strong></h3>
+                  <h3
+                    className="text-center"
+                    style={{ marginBottom: "0", fontSize: "1.42rem" }}
+                  >
+                    Total de jogadores <strong>silenciados</strong>
+                  </h3>
                 </div>
                 <div className="main-cards-body">
-                  <p className="text-center"><strong style={{fontSize: "40px", padding: "0 10px"}}>500</strong> silenciamentos.</p>
+                  <p className="text-center">
+                    <strong style={{ fontSize: "40px", padding: "0 10px" }}>
+                      500
+                    </strong>{" "}
+                    silenciamentos.
+                  </p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-          <div className="report-list"></div>
+        <div className="report-list"></div>
         <Footer />
       </>
     );
