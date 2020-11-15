0c737becb89a85e2ff4a1e321675698697e45dc5
yandv <allangamerbr252@gmail.com>
2020-11-15 14:31:58 -0300

    Change the api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create(
   /*{ baseURL: "http://api.saintmc.yallandev.tk" }*/
-  { baseURL: "https://localhost" }
+  { baseURL: "https://localhost:3333" }
 );
 
 export default api;
